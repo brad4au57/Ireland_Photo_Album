58313d204305b413d6a2784ceff87d4ec0071deb
Brad Bales <brad4au57@gmail.com>
2019-08-21 22:24:06 -0500

    Added HTML Templating

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <ul className="copyright">
+        <li>&copy; Untitled.</li>
+        <li>
+          Design: <a href="http://html5up.net">HTML5 UP</a>.
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import Header from "./components/Header";
+import Thumbnail from "./components/Thumbnail";
+import Footer from "./components/Footer";
+import "./assets/css/main.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div id="main" className="App">
+      {/* <!-- Header --> */}
+      <Header />
+      {/* <!-- Thumbnail --> */}
+      <Thumbnail />
+      {/* <!-- Footer --> */}
+      <Footer />
     </div>
   );
 }
